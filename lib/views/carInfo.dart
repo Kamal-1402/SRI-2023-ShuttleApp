b12f630be48ddbecc056703769965acfe9708505
ap217 <202001020@daiict.ac.in>
2023-06-30 16:00:44 +0530

    adichanges

--- lib/views/carInfo.dart
+++ lib/views/carInfo.dart
@@ -5,130 +5,153 @@ import 'package:flutter/material.dart';
 import '../configMaps.dart';
 import '../main.dart';
 
-
 class CarInfo extends StatelessWidget {
-   CarInfo({super.key});
+  CarInfo({super.key});
 
   static const String idScreen = "carInfo";
-  final TextEditingController carNumberTextEditingController =TextEditingController();
-  final TextEditingController carColorTextEditingController =TextEditingController();
-  final TextEditingController carModelTextEditingController =TextEditingController();
-  final TextEditingController carTypeTextEditingController =TextEditingController();
-
+  final TextEditingController carNumberTextEditingController =
+      TextEditingController();
+  final TextEditingController carColorTextEditingController =
+      TextEditingController();
+  final TextEditingController carModelTextEditingController =
+      TextEditingController();
+  final TextEditingController carTypeTextEditingController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(height: 22,),
-              Image.asset("images/Shuttle.png", width: 390, height: 250,),
-              Padding(
-                padding:EdgeInsets.fromLTRB(22, 22, 22, 32),
-                child: Column(
-                  children: [
-                    SizedBox(height: 12,),
-                    Text("Enter Shuttle Details", style: TextStyle(fontFamily: "Brand Bold", fontSize: 24),),
-                    SizedBox(height: 26,),
-                    TextField(
-                      controller: carModelTextEditingController,
-                      decoration: InputDecoration(
-                        labelText: "Shuttle Model",
-                        hintStyle: TextStyle(color: Colors.grey, fontSize: 10),
-                      ),
-                      style: TextStyle(fontSize: 15),
+          child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 22,
+            ),
+            Image.asset(
+              "images/Shuttle.png",
+              width: 390,
+              height: 250,
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(22, 22, 22, 32),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Text(
+                    "Enter Shuttle Details",
+                    style: TextStyle(fontFamily: "Brand Bold", fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 26,
+                  ),
+                  TextField(
+                    controller: carModelTextEditingController,
+                    decoration: InputDecoration(
+                      labelText: "Shuttle Model",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 10),
                     ),
-                    SizedBox(height: 10,),
-                    TextField(
-                      controller: carNumberTextEditingController,
-                      decoration: InputDecoration(
-                        labelText: "Shuttle Numbers",
-                        hintStyle: TextStyle(color: Colors.grey, fontSize: 10),
-                      ),
-                      style: TextStyle(fontSize: 15),
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: carNumberTextEditingController,
+                    decoration: InputDecoration(
+                      labelText: "Shuttle Numbers",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 10),
                     ),
-                    SizedBox(height: 10,),
-                    TextField(
-                      controller: carColorTextEditingController,
-                      decoration: InputDecoration(
-                        labelText: "Shuttle Color",
-                        hintStyle: TextStyle(color: Colors.grey, fontSize: 10),
-                      ),
-                      style: TextStyle(fontSize: 15),
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: carColorTextEditingController,
+                    decoration: InputDecoration(
+                      labelText: "Shuttle Color",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 10),
                     ),
-                    SizedBox(height: 10,),
-                    TextField(
-                      controller: carTypeTextEditingController,
-                      decoration:const InputDecoration(
-                        labelText: "Shuttle Type",
-                        hintText: "e.g. auto,van,bus",
-                        hintStyle: TextStyle(color: Colors.grey, fontSize: 10),
-                      ),
-                      style: TextStyle(fontSize: 15),
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    controller: carTypeTextEditingController,
+                    decoration: const InputDecoration(
+                      labelText: "Shuttle Type",
+                      hintText: "e.g. auto,van,bus",
+                      hintStyle: TextStyle(color: Colors.grey, fontSize: 10),
                     ),
-                    SizedBox(height: 32,),
-
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 16),
-                      child: ElevatedButton(
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16),
+                    child: ElevatedButton(
                         onPressed: () {
-                          if(carModelTextEditingController.text.isEmpty){
-                            displayToastMessage("Please write Shuttle Model", context);
-                          }
-                          else if(carNumberTextEditingController.text.isEmpty){
-                            displayToastMessage("Please write Shuttle Number", context);
-                          }
-                          else if(carColorTextEditingController.text.isEmpty){
-                            displayToastMessage("Please write Shuttle Color", context);
-                          }
-                          else if(carTypeTextEditingController.text.isEmpty){
-                            displayToastMessage("Please write Shuttle Type", context);
-                          }
-                          else{
+                          if (carModelTextEditingController.text.isEmpty) {
+                            displayToastMessage(
+                                "Please write Shuttle Model", context);
+                          } else if (carNumberTextEditingController
+                              .text.isEmpty) {
+                            displayToastMessage(
+                                "Please write Shuttle Number", context);
+                          } else if (carColorTextEditingController
+                              .text.isEmpty) {
+                            displayToastMessage(
+                                "Please write Shuttle Color", context);
+                          } else if (carTypeTextEditingController
+                              .text.isEmpty) {
+                            displayToastMessage(
+                                "Please write Shuttle Type", context);
+                          } else {
                             saveDriverCarInfo(context);
                           }
                         },
                         style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.black, backgroundColor: Colors.yellow,
+                          foregroundColor: Colors.black,
+                          backgroundColor: Colors.yellow,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(24),
                           ),
                         ),
-                        child:const Padding(
-                          padding: EdgeInsets.all(17),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "Next",
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  fontFamily: "Brand Bold",
+                        child: const Padding(
+                            padding: EdgeInsets.all(17),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Next",
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontFamily: "Brand Bold",
+                                  ),
                                 ),
-                              ),
-                              Icon(
-                                Icons.arrow_forward,
-                                color: Colors.black,
-                                size: 26,
-                              ),
-                            ],
-                          )
-                        )
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        )
+                                Icon(
+                                  Icons.arrow_forward,
+                                  color: Colors.black,
+                                  size: 26,
+                                ),
+                              ],
+                            ))),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
+      )),
     );
   }
 
-  void saveDriverCarInfo(BuildContext context){
+  void saveDriverCarInfo(BuildContext context) {
     String userId = currentfirebaseUser!.uid;
     Map carInfoMap = {
       "car_color": carColorTextEditingController.text,
@@ -137,9 +160,11 @@ class CarInfo extends StatelessWidget {
       "type": carTypeTextEditingController.text,
     };
     driversRef.child(userId).child("car_details").set(carInfoMap);
-    Navigator.pushNamedAndRemoveUntil(context, "/Home/MapPage/", (route) => false);
+    Navigator.pushNamedAndRemoveUntil(
+        context, "/Home/MapGoogle/", (route) => false);
   }
 }
+
 void displayToastMessage(String message, BuildContext context) {
   ScaffoldMessenger.of(context).showSnackBar(
     SnackBar(
